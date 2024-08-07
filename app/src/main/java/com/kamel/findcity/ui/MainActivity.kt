e40b0e1c339035d4c9c7957b98313727fe139b17
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2024-08-07 16:20:08 +0300

    add ui layer

---
+++ app/src/main/java/com/kamel/findcity/ui/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.kamel.findcity.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.kamel.findcity.ui.theme.FindCityTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FindCityTheme {
+            }
+        }
+    }
+}
