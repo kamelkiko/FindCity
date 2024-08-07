1b5f786b3aa15881024fa19898edd922bf127785
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2024-08-08 02:34:41 +0300

    add Typography

--- app/src/main/java/com/kamel/findcity/ui/theme/Type.kt
+++ app/src/main/java/com/kamel/findcity/ui/theme/Type.kt
@@ -1,34 +1,75 @@
 package com.kamel.findcity.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import com.kamel.findcity.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+@Composable
+fun headline(): TextStyle {
+    return TextStyle(
         fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        lineHeight = 34.5.sp,
+        fontFamily = FontFamily(Font(R.font.roboto_bold)),
+        fontWeight = FontWeight.W400,
+        textAlign = TextAlign.Center,
     )
-    */
-)
+}
+
+@Composable
+fun titleLarge(): TextStyle {
+    return TextStyle(
+        fontSize = 18.sp,
+        lineHeight = 20.sp,
+        fontFamily = FontFamily(Font(R.font.roboto_medium)),
+        fontWeight = FontWeight.W400,
+        textAlign = TextAlign.Center,
+    )
+}
+
+@Composable
+fun title(): TextStyle {
+    return TextStyle(
+        fontSize = 16.sp,
+        fontFamily = FontFamily(Font(R.font.roboto_medium)),
+        fontWeight = FontWeight.W400,
+        lineHeight = 22.04.sp,
+        textAlign = TextAlign.Center,
+    )
+}
+
+@Composable
+fun titleMedium(): TextStyle {
+    return TextStyle(
+        fontSize = 14.sp,
+        fontFamily = FontFamily(Font(R.font.roboto_medium)),
+        fontWeight = FontWeight.W400,
+        lineHeight = 22.04.sp,
+        textAlign = TextAlign.Center,
+    )
+}
+
+@Composable
+fun body(): TextStyle {
+    return TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 22.43.sp,
+        fontFamily = FontFamily(Font(R.font.roboto_medium)),
+        fontWeight = FontWeight.W400,
+        textAlign = TextAlign.Center,
+    )
+}
+
+@Composable
+fun caption(): TextStyle {
+    return TextStyle(
+        fontFamily = FontFamily(Font(R.font.roboto_regular)),
+        fontWeight = FontWeight.W400,
+        fontSize = 12.sp,
+        lineHeight = 14.5.sp,
+    )
+}
---
+++ app/src/main/java/com/kamel/findcity/ui/theme/Typography.kt
@@ -0,0 +1,12 @@
+package com.kamel.findcity.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+
+data class Typography(
+    val headline: TextStyle = TextStyle(),
+    val titleLarge: TextStyle = TextStyle(),
+    val title: TextStyle = TextStyle(),
+    val titleMedium: TextStyle = TextStyle(),
+    val body: TextStyle = TextStyle(),
+    val caption: TextStyle = TextStyle(),
+)
