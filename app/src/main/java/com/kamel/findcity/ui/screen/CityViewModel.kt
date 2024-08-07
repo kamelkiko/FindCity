d0e663e3a9eb051dad2714e536fb072dccdaf3e2
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2024-08-07 17:44:15 +0300

    update CityViewModel to handle ErrorState

--- app/src/main/java/com/kamel/findcity/ui/screen/CityViewModel.kt
+++ app/src/main/java/com/kamel/findcity/ui/screen/CityViewModel.kt
@@ -5,8 +5,14 @@ import androidx.lifecycle.viewModelScope
 import com.kamel.findcity.domain.entity.City
 import com.kamel.findcity.domain.usecase.GetAllCitiesUseCase
 import com.kamel.findcity.domain.usecase.SearchCitiesByPrefixUseCase
+import com.kamel.findcity.domain.util.JsonFileNotFoundException
+import com.kamel.findcity.domain.util.NotFoundException
+import com.kamel.findcity.domain.util.UnknownErrorException
+import com.kamel.findcity.ui.screen.util.ErrorState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -27,39 +33,20 @@ class CityViewModel @Inject constructor(
     }
 
     private fun fetchAllCities() {
-        viewModelScope.launch(Dispatchers.Default) {
-            try {
-                startLoading()
-                val cities = getAllCities.invoke()
-                stopLoadingWithData(cities)
-            } catch (e: Exception) {
-                showError(e.message.toString())
-            }
-        }
-    }
-
-    fun searchForCityByName(name: String) {
-        viewModelScope.launch(Dispatchers.Default) {
-            try {
-                startLoading()
-                val cities = searchCities.invoke(name)
-                stopLoadingWithData(cities)
-            } catch (e: Exception) {
-                showError(e.message.toString())
-            }
-        }
-    }
-
-    private fun startLoading() {
         _state.update {
             it.copy(
                 isLoading = true,
                 error = ""
             )
         }
+        tryToExecute(
+            function = getAllCities::invoke,
+            onSuccess = ::onFetchAllCitiesSuccess,
+            onError = ::handleError
+        )
     }
 
-    private fun stopLoadingWithData(cities: List<City>) {
+    private fun onFetchAllCitiesSuccess(cities: List<City>) {
         _state.update {
             it.copy(
                 isLoading = false,
@@ -69,12 +56,80 @@ class CityViewModel @Inject constructor(
         }
     }
 
-    private fun showError(errorMessage: String) {
+    fun searchForCityByName(name: String) {
+        _state.update {
+            it.copy(
+                isLoading = true,
+                error = ""
+            )
+        }
+        tryToExecute(
+            function = { searchCities.invoke(name) },
+            onSuccess = ::onSearchForCityByNameSuccess,
+            onError = ::handleError
+        )
+    }
+
+    private fun onSearchForCityByNameSuccess(cities: List<City>) {
         _state.update {
             it.copy(
                 isLoading = false,
-                error = errorMessage
+                error = "",
+                cities = cities.map { city -> city.toUiState() }
             )
+        }
+    }
+
+    private fun handleError(error: ErrorState) {
+        when (error) {
+            is ErrorState.JsonFileNotFound -> {
+                _state.update {
+                    it.copy(
+                        isLoading = false,
+                        error = error.message.toString()
+                    )
+                }
+            }
+
+            is ErrorState.NotFound -> {
+                _state.update {
+                    it.copy(
+                        isLoading = false,
+                        error = error.message.toString()
+                    )
+                }
+            }
+
+            is ErrorState.UnknownError -> {
+                _state.update {
+                    it.copy(
+                        isLoading = false,
+                        error = error.message.toString()
+                    )
+                }
+            }
+        }
+    }
+
+    private fun <T> tryToExecute(
+        function: suspend () -> T,
+        onSuccess: (T) -> Unit,
+        onError: (ErrorState) -> Unit,
+        dispatcher: CoroutineDispatcher = Dispatchers.Default,
+    ): Job {
+        return viewModelScope.launch(dispatcher) {
+            try {
+                val result = function()
+                onSuccess(result)
+            } catch (exception: NotFoundException) {
+                onError(ErrorState.NotFound(exception.message))
+            } catch (exception: JsonFileNotFoundException) {
+                onError(ErrorState.JsonFileNotFound(exception.message))
+            } catch (exception: UnknownErrorException) {
+                onError(ErrorState.UnknownError(exception.message))
+            } catch (exception: Exception) {
+                onError(ErrorState.UnknownError(exception.message))
+            }
         }
     }
 }
