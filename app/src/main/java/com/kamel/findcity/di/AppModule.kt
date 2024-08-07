b211679826aa1d60d697ace1041380efbab7d2ee
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2024-08-07 17:59:25 +0300

    add AppModule in di to provide Context

---
+++ app/src/main/java/com/kamel/findcity/di/AppModule.kt
@@ -0,0 +1,19 @@
+package com.kamel.findcity.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AppModule {
+    @Singleton
+    @Provides
+    fun provideContext(@ApplicationContext context: Context): Context {
+        return context
+    }
+}
