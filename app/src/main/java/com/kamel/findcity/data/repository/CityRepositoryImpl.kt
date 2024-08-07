ec531dbeaeb1c066cf5af4b8f099c56b25443ce4
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2024-08-07 16:36:30 +0300

    add CityRepositoryImpl and LocalDataSource

---
+++ app/src/main/java/com/kamel/findcity/data/repository/CityRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package com.kamel.findcity.data.repository
+
+import com.kamel.findcity.data.local.mapper.toEntity
+import com.kamel.findcity.data.repository.local.LocalDataSource
+import com.kamel.findcity.domain.entity.City
+import com.kamel.findcity.domain.repository.CityRepository
+import javax.inject.Inject
+
+class CityRepositoryImpl @Inject constructor(
+    private val localDataSource: LocalDataSource
+) : CityRepository {
+    override suspend fun getAllCities(): List<City> {
+        return localDataSource.getAllCities().map { it.toEntity() }
+    }
+
+    override fun searchCityByPrefix(prefix: String): List<City> {
+        return localDataSource.searchCityByPrefix(prefix).map { it.toEntity() }
+    }
+}
---
+++ app/src/main/java/com/kamel/findcity/data/repository/local/LocalDataSource.kt
@@ -0,0 +1,8 @@
+package com.kamel.findcity.data.repository.local
+
+import com.kamel.findcity.data.local.model.CityDto
+
+interface LocalDataSource {
+    suspend fun getAllCities(): List<CityDto>
+    fun searchCityByPrefix(prefix: String): List<CityDto>
+}
