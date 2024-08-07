f188497138d834df15f477c0329f84621e57ff10
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2024-08-07 18:00:03 +0300

    fix issue

--- app/src/main/java/com/kamel/findcity/di/AppModule.kt
+++ app/src/main/java/com/kamel/findcity/di/AppModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class AppModule {
+object AppModule {
     @Singleton
     @Provides
     fun provideContext(@ApplicationContext context: Context): Context {
