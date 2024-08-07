bc1bec7c0e020e7f5e71152f46cd689a1db1fa37
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2024-08-08 01:48:42 +0300

    remove insertCityToTrie function from CityRepositoryImpl

--- app/src/main/java/com/kamel/findcity/data/repository/CityRepositoryImpl.kt
+++ app/src/main/java/com/kamel/findcity/data/repository/CityRepositoryImpl.kt
@@ -2,22 +2,15 @@ package com.kamel.findcity.data.repository
 
 import com.kamel.findcity.data.local.mapper.toEntity
 import com.kamel.findcity.data.repository.local.LocalDataSource
-import com.kamel.findcity.domain.ds.TrieCity
 import com.kamel.findcity.domain.entity.City
 import com.kamel.findcity.domain.repository.CityRepository
 import javax.inject.Inject
 
 class CityRepositoryImpl @Inject constructor(
     private val localDataSource: LocalDataSource,
-    private val trieCity: TrieCity,
 ) : CityRepository {
     override suspend fun getAllCities(): List<City> {
-        return localDataSource.getAllCities().map { it.toEntity() }.onEach { city ->
-            insertCityToTrie(city)
-        }
+        return localDataSource.getAllCities().map { it.toEntity() }
     }
 
-    private fun insertCityToTrie(city: City) {
-        trieCity.insert(city.name.lowercase(), city)
-    }
 }
